7da67f61f050e9c27555b3f9af6b2ca6c81d996a
Floris Remmen <floris.remmen@gmail.com>
2020-09-25 11:54:07 +0200

    initial figure generation

--- visualiser/visualiser.py
+++ visualiser/visualiser.py
@@ -7,15 +7,19 @@
 # Date           : "22 September 2020"                                     
 ###################################################################
 
+import matplotlib.pyplot as plt
+import pandas as pd
+import geopandas as gpd
+
 class Visualiser: 
 
-    def __init__(self): 
+    def __init__(self):
         pass
 
     def __repr__(self): 
         return "Visualiser class for mi-code-challenge"
 
-    def visualise(self, simulation_results: dict): 
+    def generate_figure(self, simulation_results: dict): 
         """
         Visualises the results given in the simulation dictionary
         :type simulation_results: dict
@@ -24,9 +28,19 @@ class Visualiser:
 
         # Sim results look like this 
         # {
-        #     'booking_distance_bins': booking_distance_bins,
+        #     'booking_distance_bins': booking_distance_bins, 
+        #           -->  get_booking_distance_bins
+        #           --> {'From 0->1km': 1, 'From 1->2km': 1, 'From 2->3km': 2, 'From 3->4km': 2}
         #     'most_popular_dropoff_points': most_popular_dropoff_points,
-        #     'most_popular_pickup_points': most_popular_pickup_points
+        #           --> get_random_points
+        #           --> geodataframe with (index, name, id, geom) 
+        #     'most_popular_pickup_points': most_popular_pickup_points --> get_random_points
+        #           --> get_random_points
+        #           --> geodataframe with (index, name, id, geom) and size number_of_requests
         # }
 
-        return 0
+        # f, ax = plt.figure() 
+
+        simulation_results['most_popular_dropoff_points'].plot() 
+
+        plt.savefig('webapp/static/plot.png')
