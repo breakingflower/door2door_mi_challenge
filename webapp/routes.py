926b7cb9d40f7e774e4fb215644a225245b1c59b
Floris Remmen <floris.remmen@gmail.com>
2020-09-25 16:47:35 +0200

    updated visualise to use unique identifier

--- webapp/routes.py
+++ webapp/routes.py
@@ -7,6 +7,8 @@ from webapp.forms import TriggerForm
 from simulator.simulator import Simulator
 from visualiser.visualiser import Visualiser
 
+import random 
+
 routes = Blueprint('routes', __name__)
 
 @routes.before_request
@@ -20,22 +22,27 @@ def before_request():
         code = 301
         return redirect(url, code=code)
 
-@routes.route('/visualise')
-def visualise():
+@routes.route('/visualise/<int:viz_id>')
+def visualise(viz_id):
     """
-    Renders a page with the image located in /webapp/static/plot.png.
-    The image is made prior to this route, therefore it will be the last generated
-    simulation.
+    Renders a page with generated images.
+    The image is made prior to this route, and an ID is prepended to the filename.
+    The id is passed as a parameter.
+    :type viz_id: int
     """
-    return render_template('visualise.html', image_url='/webapp/static/plot.png')
+
+    return render_template('visualise.html',
+            overview_image_url= url_for('static', filename=f'{viz_id}_overview_plot.png'), 
+            closeup_image_url=url_for('static', filename=f'{viz_id}_closeup_plot.png'),
+            gmap_url=url_for('static', filename=f'{viz_id}_map.html'))
 
 @routes.route('/', methods=['GET', 'POST'])
-def home():
+def trigger_page():
     """
-    Renders the home page. This page contains an instance of a TriggerForm.
+    Renders the trigger page. This page contains an instance of a TriggerForm.
     The TriggerForm contains input fields for the simulation. If the submit
     button is pressed, a simulation is triggered and the results are visualised
-    using the Simulator and Visualiser class.
+    using the Visualiser class.
     """
     form = TriggerForm() 
 
@@ -49,12 +56,15 @@ def home():
                         form.x2_field.data, form.y2_field.data)
 
         # simulator instance creation
-        simulation_result = Simulator(bounding_box).simulate(number_of_requests)
+        simulation_results = Simulator(bounding_box).simulate(number_of_requests)
 
-        # generate a figure
-        Visualiser().generate_figure(simulation_result)
+        # generate a static figure
+        viz = Visualiser(bounding_box, simulation_results)
+        viz.generate_overview_figure()
+        viz.generate_closeup_figure()
+        viz.generate_gmap()
         
         # redirect to the visualise endpoint
-        return redirect(url_for('routes.visualise'))
+        return redirect(url_for('routes.visualise', viz_id = viz.id))
 
     return render_template('home.html', title="MI Code Challenge", form=form)
