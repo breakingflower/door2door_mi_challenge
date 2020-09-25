2cba7d81fe87580669423a3ab7609648ae97f599
Floris Remmen <floris.remmen@gmail.com>
2020-09-25 11:49:27 +0200

    reads static data files, initial commit

---
+++ utilities/staticdatareader.py
@@ -0,0 +1,34 @@
+import pandas as pd 
+import geopandas as gpd 
+
+class StaticDataReader: 
+    """
+    Reads the static data files
+    """
+
+    def __init__(self): 
+
+        # This can be improved by not hardcoding
+        self.berlin_bounds_file = 'data/berlin_bounds.poly'
+        self.berlin_stops_file  = 'data/berlin_stops.geojson'
+        
+        # read files 
+        self.berlin_stops = self._read_berlin_stops()
+        self.berlin_bounds = self._read_berlin_bounds()
+
+    def _read_berlin_stops(self): 
+        """
+        Reads berlin stops from a geojson file
+        :rtype geopandas.GeoDataFrame
+        """
+        return gpd.read_file(self.berlin_stops_file)
+
+    def _read_berlin_bounds(self): 
+        """
+        Reads berlin bounds from a poly file. 
+        :rtype geopandas.GeoDataFrame
+        """
+        df = pd.read_csv(self.berlin_bounds_file, delim_whitespace=True, header=None)
+        df.columns = ['lat', 'lon']
+
+        return gpd.GeoDataFrame(geometry = gpd.points_from_xy(df.lat, df.lon))
