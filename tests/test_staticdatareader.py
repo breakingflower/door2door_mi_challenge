bc6972af51acc3080a127ece53c2c1a67e1548b2
Floris Remmen <floris.remmen@gmail.com>
2020-09-28 19:26:07 +0200

    adds staticdatareader test

---
+++ tests/test_staticdatareader.py
@@ -0,0 +1,50 @@
+import unittest
+from utilities.staticdatareader import StaticDataReader
+
+class TestStaticDataReader(unittest.TestCase): 
+    """
+    Testcase for the Visualiser Class
+    """
+    @classmethod
+    def setUpClass(self): 
+        """
+        Sets up a case where good filenames are given and a case
+        where bad filenames are given.
+        """
+
+        self.static_data_good = StaticDataReader(
+            'data/berlin_bounds.poly', 
+            'data/berlin_stops.geojson'
+        )
+        self.static_data_bad = StaticDataReader(
+            'somerandomfile.jpeg', 
+            'someotherfile.pngx'
+        )
+        
+    def setUp(self): 
+        pass
+
+    def test_read_berlin_bounds(self): 
+        """
+        Tests output of _read_berlin_bounds().
+        The instance where the files exist is not empty
+        The ones where bogus filenames are used should be empty.
+        """
+        self.assertFalse(self.static_data_good.berlin_bounds.empty)
+        self.assertTrue(self.static_data_bad.berlin_bounds.empty)
+
+    def test_read_berlin_stops(self): 
+        """
+        Similar to test_read_berlin_bounds.
+        Tests output of _read_berlin_bounds().
+        The instance where the files exist is not empty
+        The ones where bogus filenames are used should be empty.
+        """
+        self.assertFalse(self.static_data_good.berlin_stops.empty)
+        self.assertTrue(self.static_data_bad.berlin_stops.empty)
+
+    def tearDown(self): 
+        pass
+
+    if __name__ == "__main__":
+        unittest.main()
