44782aa61c6b0315681d1d7a16cc6b8a04cc43ca
Floris Remmen <floris.remmen@gmail.com>
2020-09-28 18:07:53 +0200

    adds check to see if file exists

--- utilities/staticdatareader.py
+++ utilities/staticdatareader.py
@@ -1,6 +1,8 @@
 import pandas as pd 
 import geopandas as gpd 
 
+from os.path import isfile
+
 class StaticDataReader: 
     """
     Reads the static data files
@@ -21,16 +23,24 @@ class StaticDataReader:
 
     def _read_berlin_stops(self) -> gpd.GeoDataFrame: 
         """
-        Reads berlin stops from a geojson file, with epsg 4326
+        Reads berlin stops from a geojson file, with epsg 4326. 
+        If the file does not exist, return an empty geodataframe.
         :rtype geopandas.GeoDataFrame
         """
+
+        if not isfile(self.berlin_stops_file): 
+            return gpd.GeoDataFrame()
         return gpd.read_file(self.berlin_stops_file, crs='epsg:4326')
 
     def _read_berlin_bounds(self) -> gpd.GeoDataFrame: 
         """
-        Reads berlin bounds from a poly file and set epsg to 4326
+        Reads berlin bounds from a poly file and set epsg to 4326.
+        If the file does not exist, return an empty geodataframe.
         :rtype geopandas.GeoDataFrame
         """
+        if not isfile(self.berlin_bounds_file): 
+            return gpd.GeoDataFrame()
+
         df = pd.read_csv(self.berlin_bounds_file, delim_whitespace=True, header=None)
         df.columns = ['lat', 'lon']
 
