9bccde9309efe1cc7b2c85fecaa7224c7f9746c8
Floris Remmen <floris.remmen@gmail.com>
2020-09-28 16:50:17 +0200

    code clean

--- utilities/cleanup.py
+++ utilities/cleanup.py
@@ -1,22 +1,17 @@
 import os
 import glob
 
-from flask import current_app
-
 class Cleaner:
     """
     Class to clean up the results of the simulation / visualisation
     """
 
     @staticmethod
-    def remove_previous_simulation_results(): 
+    def remove_previous_simulation_results(static_path: str): 
         """
         Removes all files with the .png and .html extension from the static folder
         """
 
-        # get the full path from the current_app
-        static_path = os.path.join(current_app.root_path, current_app.static_folder)
-        
         previous_simulation_images = glob.glob(f'{static_path}/*.png')
         previous_simulation_html = glob.glob(f'{static_path}/*.html')
 
