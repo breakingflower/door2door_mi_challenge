7f260994e0de4e213eda33a155b33ecf5e31fa2c
Floris Remmen <floris.remmen@gmail.com>
2020-09-28 11:20:25 +0200

    adds bounding box class

---
+++ utilities/bounding_box.py
@@ -0,0 +1,42 @@
+from pyproj import Proj, transform
+
+class BoundingBox: 
+    """
+    Class for bounding box. Essentially a property class: center, lats, lons, ...
+    """
+
+    def __init__(self, bounding_box: tuple): 
+        
+        # bounding box is x1, y1, x2, y2
+        self.bounding_box = bounding_box
+    
+    @property
+    def center(self): 
+        """
+        Returns a tuple with the center of the bounding box, calculated by averaging lat/lon coordinates.
+        eg (y0 + y1) / 2 , (x0 + x1) / 2
+        """
+        return (self.bounding_box[1] + self.bounding_box[3]) / 2, (self.bounding_box[0] + self.bounding_box[2]) / 2
+    
+    def to_crs(self, epsg=3857): 
+        in_proj = Proj(init='epsg:4326')
+        out_proj = Proj(init=f'epsg:{epsg}')
+
+        x1_proj, y1_proj = transform(in_proj, out_proj, self.bounding_box[0], self.bounding_box[1])
+        x2_proj, y2_proj = transform(in_proj, out_proj, self.bounding_box[2], self.bounding_box[3])
+
+        return x1_proj, y1_proj, x2_proj, y2_proj
+
+    @property
+    def lats(self): 
+        """
+        Returns list of latitude coordinates
+        """
+        return [self.bounding_box[1], self.bounding_box[3], self.bounding_box[3], self.bounding_box[1]]
+    
+    @property
+    def lons(self):
+        """
+        Returns list of longitude coordinates
+        """
+        return [self.bounding_box[0], self.bounding_box[0], self.bounding_box[2], self.bounding_box[2]]
